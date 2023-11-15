459afe115506a85eb455de933f1ac098e101fbaa
zeinhasan <ahmad.habib.hasan@mail.ugm.ac.id>
2023-11-15 16:16:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from model import chatbot
 
 # pylint: disable=C0103
 app = Flask(__name__)
+
 def get_user_timezone(offset):
     offset_timedelta = timedelta(minutes=offset)
     utc_time = datetime.utcnow()
@@ -16,24 +17,35 @@ def get_user_timezone(offset):
     return user_timezone
 
 @app.route('/predict', methods=['POST'])
-def predict(chatuser):
-  data = request.get_json()
-  prompt = data.get('prompt')
-  offset = data.get('offset')
-  if not prompt:
-    return jsonify({"Response": "Halo! Selamat datang di Zein Virtual Asisten. Bagaimana saya bisa membantu Anda hari ini ?"}), 200
-  try:
-     user_timezone_offset = int(offset)
-  except (TypeError, ValueError):
-     return jsonify({'error': 'Invalid offset parameter'}), 400
+def predict():
+    # Get API key from request headers
+    api_key = request.headers.get('API-Key')
 
-  user_timezone = get_user_timezone(user_timezone_offset)
+    # Verify API key
+    if api_key != VALID_API_KEY:
+        return jsonify({"error": "Invalid API Key"}), 401
 
-  prediction = chatbot(prompt)
+    data = request.get_json()
+    prompt = data.get('prompt')
+    offset = data.get('offset')
+    
+    if not prompt:
+        return jsonify({"Response": "Halo! Selamat datang di Zein Virtual Asisten. Bagaimana saya bisa membantu Anda hari ini ?"}), 200
 
-  return {
+    try:
+        user_timezone_offset = int(offset*60)
+    except (TypeError, ValueError):
+        return jsonify({'error': 'Invalid offset parameter'}), 400
+
+    user_timezone = get_user_timezone(user_timezone_offset)
+
+    prediction = chatbot(prompt)
+
+    return {
         "Prompt": prompt,
         "Response": prediction,
         "UserTimezone": user_timezone.strftime("%Y-%m-%d %H:%M:%S %Z")
     }
 
+if __name__ == '__main__':
+    app.run(debug=True)
